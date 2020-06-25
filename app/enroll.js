bcf2fad2c9167cecda15714302e239029d925e69
kozyilmaz <kazim@monolytic.com>
2020-06-25 21:38:51 +0300

    enroll fixes

--- app/enroll.js
+++ app/enroll.js
@@ -1,14 +1,17 @@
-import { createWeb3Provider, getEtherBalance, getAccountByIndex, getNetworkName, getProviderName } from './common.js';
+import { xyz_get_contract, xyz_create_web3_provider, xyz_get_balance, xyz_get_account_by_index, xyz_get_network_name, xyz_get_provider_name } from './common.js';
 
 // default account
 var account;
+// smart contract
+var contract_abi;
+var contract_address;
 
 window.App = {
 	start: async () => {
 		var self = this;
 
 		// get provider name
-		const provider = await getProviderName();
+		const provider = await xyz_get_provider_name();
 		if (provider === null) {
 				alert("Couldn't get any Web3 providers, probably Metamask/Mist/Infura is not present!");
 				return;
@@ -20,40 +23,41 @@ window.App = {
 			document.getElementById("providernote").innerHTML = "Metamask is NOT available, please use a local node or set 'infuraAPIKey' and connect to Rinkeby network! Some functionality will not work without Metamask";
 		}
 		// get network name
-		const network = await getNetworkName();
+		const network = await xyz_get_network_name();
 		if (network === null) {
 			alert("Couldn't get any the Ethereum network, probably Metamask/Mist/Infura is not present!");
 			return;
 		}
 		// get default account
-		account = await getAccountByIndex(0);
+		account = await xyz_get_account_by_index(0);
 		if (account === null) {
-			alert("Couldn't get the default Ethereum accounts, probably Metamask/Mist/Infura is not present!");
-			return;
+			document.getElementById("defaultaddress").innerHTML = "invalid";
+		} else {
+			document.getElementById("defaultaddress").innerHTML = account;
 		}
-		document.getElementById("defaultaddress").innerHTML = account;
 
 		// get balance
-		const balance = await getEtherBalance(account);
+		const balance = await xyz_get_balance(account);
 		if (balance === null) {
 			document.getElementById("etherbalanceauto").innerHTML = "invalid";
 		} else {
 			document.getElementById("etherbalanceauto").innerHTML = web3.utils.fromWei(balance, 'ether');
 		}
+
+		var contractvars = await xyz_get_contract("YKTS.json");
+		contract_address = contractvars[0];
+		contract_abi = contractvars[1];
+
+		console.log("YKTS Contract Address", contract_address);
+		console.log("YKTS Contract ABI", contract_abi);
+
+		//const contract = new web3.eth.Contract(contractJSON.abi, deployedAddress);
 	},
-
-  // send ether
-  sendEther: function() {
-    var self = this;
-  },
-
-  // send ERC20 token
-
 
 };
 
 // hooking up web3 provider
 window.addEventListener('load', async () => {
-  createWeb3Provider();
+	xyz_create_web3_provider();
 	App.start();
 });

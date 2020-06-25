d479b18fb183349baff41a941afee7da9dc3158b
kozyilmaz <kazim@monolytic.com>
2020-06-25 21:36:32 +0300

    common update

--- app/common.js
+++ app/common.js
@@ -1,7 +1,32 @@
 
-let infuraProjectId = "17816af5e26d434aa2f6d52915764ed7";
+export async function xyz_get_contract(contract_name) {
+	// parse compiled smart contract JSON
+	var contract_json;
+	$.ajax({
+		url: contract_name,
+		dataType: 'json',
+		async: false,
+		data: {},
+		success: function(data) {
+			contract_json = data;
+		},
+		error: function (error) {
+			return [null, null];
+		}
+	});
+	// get network id
+	const networkId = await web3.eth.net.getId()
 
-export async function getAccountByIndex(index) {
+	const contract_address = contract_json.networks[networkId.toString()].address;
+	const contract_abi = contract_json.abi;
+
+	console.log("Smart Contract", contract_name, "Address", contract_address);
+	console.log("Smart Contract", contract_name, "ABI", contract_abi);
+
+	return [contract_address, contract_abi];
+}
+
+export async function xyz_get_account_by_index(index) {
 	var account;
 	var accounts;
 	try {
@@ -22,7 +47,7 @@ export async function getAccountByIndex(index) {
 	return account;
 }
 
-export async function getAccounts() {
+export async function xyz_get_accounts() {
 	var accounts;
 	try {
 		accounts = await web3.eth.getAccounts();
@@ -37,7 +62,7 @@ export async function getAccounts() {
 	return accounts;
 }
 
-export async function getEtherBalance(walletAddress) {
+export async function xyz_get_balance(walletAddress) {
 	try {
 		if (web3.utils.isAddress(walletAddress) != true) {
 			console.log("invalid Ethereum address");
@@ -52,7 +77,7 @@ export async function getEtherBalance(walletAddress) {
 	}
 }
 
-export async function getNetworkName() {
+export async function xyz_get_network_name() {
 	try {
 		const network = await web3.eth.net.getNetworkType();
 		console.log("Ethereum Network", network);
@@ -64,7 +89,7 @@ export async function getNetworkName() {
 }
 
 // copied from https://ethereum.stackexchange.com/questions/24266/elegant-way-to-detect-current-provider-int-web3-js
-export async function getProviderName() {
+export async function xyz_get_provider_name() {
 	if (window.web3.currentProvider.isMetaMask)
 		return 'metamask';
 	if (window.web3.currentProvider.isTrust)
@@ -84,7 +109,9 @@ export async function getProviderName() {
 	return null;
 }
 
-export async function createWeb3Provider() {
+const infura_project_id = "17816af5e26d434aa2f6d52915764ed7";
+
+export async function xyz_create_web3_provider() {
 	// Modern dapp browsers...
 	if (window.ethereum) {
 		console.log('Modern DApp Browser Detected! ' + web3.currentProvider.constructor.name)
@@ -107,14 +134,14 @@ export async function createWeb3Provider() {
 	// Non-dapp browsers...
 	else {
 		// fallback - infura or localhost
-		if (!infuraProjectId || infuraProjectId === "") {
+		if (!infura_project_id || infura_project_id === "") {
 			// use local node
 			console.log('No Web3 Detected... trying Geth')
 			window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 		} else {
 			// use Infura
 			console.log('No Web3 Detected... trying Infura (on Rinkeby)')
-			window.web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/v3/" + infuraProjectId));
+			window.web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/v3/" + infura_project_id));
 		}
 	}
 }

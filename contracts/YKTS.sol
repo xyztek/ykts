76ab9ac10595975197e1de2d9c8d9829f9a2262d
kozyilmaz <kazim@monolytic.com>
2020-06-25 14:21:06 +0300

    fix import

--- contracts/YKTS.sol
+++ contracts/YKTS.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.0;
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import "openzeppelin-solidity/contracts/access/AccessControl.sol";
 
 /**
  * @title YKTSContract

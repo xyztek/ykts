c2db3d5423edea829109d164312e5640f97bd162
kozyilmaz <kazim@monolytic.com>
2020-06-30 13:28:20 +0300

    admins

--- app/admin.js
+++ app/admin.js
@@ -1,4 +1,4 @@
-import { xyz_create_web3_provider, xyz_get_account_by_index, xyz_get_network_name, xyz_get_provider_name, xyz_get_accounts } from './common.js';
+import { xyz_create_web3_provider, xyz_get_account_by_index, xyz_get_network_name, xyz_get_provider_name } from './common.js';
 import { xyz_ykts_get_contract, xyz_ykts_add_admin, xyz_ykts_renounce_admin } from './ykts.js';
 
 // YKTS contract interface
@@ -77,6 +77,7 @@ window.App = {
 		}
 		// add admin
 		const response = await xyz_ykts_add_admin(ykts_contract, sender_address, new_admin_address);
+		// TODO error handling
 		console.log("Admin Add:", response);
 		// OK
 		document.getElementById("add_admin_status").innerHTML = "OK";
@@ -95,12 +96,71 @@ window.App = {
 			alert("Unable to get default address, aborting!");
 			return;
 		}
-		// add admin
+		// renounce yourself from admin priviliges
 		const response = await xyz_ykts_renounce_admin(ykts_contract, sender_address);
+		// TODO error handling
 		console.log("Admin Renounce:", response);
 		// OK
 		document.getElementById("renounce_admin_status").innerHTML = "OK";
 		document.getElementById("renounce_admin_sender").innerHTML = sender_address;
+	},
+
+	add_notary: async () => {
+		var self = this;
+		document.getElementById("add_notary_status").innerHTML = "Pending";
+		document.getElementById("add_notary_sender").innerHTML = 0;
+
+		// get tx sender address (current admin!)
+		const sender_address = await xyz_get_account_by_index(0);
+		if (sender_address == null) {
+			document.getElementById("add_notary_status").innerHTML = "Failed";
+			alert("Unable to get default address, aborting!");
+			return;
+		}
+		// get new notary address
+		const new_notary_address = document.getElementById('new_notary_address').value;
+		if (!new_notary_address) {
+			document.getElementById("add_notary_status").innerHTML = "Failed";
+			document.getElementById("add_notary_sender").innerHTML = sender_address;
+			alert("Empty admin address field!");
+			return;
+		}
+		// add notary
+		const response = await xyz_ykts_add_notary(ykts_contract, sender_address, new_notary_address);
+		// TODO error handling
+		console.log("Notary Add:", response);
+		// OK
+		document.getElementById("add_notary_status").innerHTML = "OK";
+		document.getElementById("add_notary_sender").innerHTML = sender_address;
+	},
+
+	remove_notary: async () => {
+		var self = this;
+		document.getElementById("remove_notary_status").innerHTML = "Pending";
+		document.getElementById("remove_notary_sender").innerHTML = 0;
+
+		// get tx sender address (current admin!)
+		const sender_address = await xyz_get_account_by_index(0);
+		if (sender_address == null) {
+			document.getElementById("remove_notary_status").innerHTML = "Failed";
+			alert("Unable to get default address, aborting!");
+			return;
+		}
+		// get new notary address
+		const remove_notary_address = document.getElementById('remove_notary_address').value;
+		if (!remove_notary_address) {
+			document.getElementById("remove_notary_status").innerHTML = "Failed";
+			document.getElementById("remove_notary_sender").innerHTML = sender_address;
+			alert("Empty admin address field!");
+			return;
+		}
+		// add notary
+		const response = await xyz_ykts_remove_notary(ykts_contract, sender_address, remove_notary_address);
+		// TODO error handling
+		console.log("Notary Remove:", response);
+		// OK
+		document.getElementById("remove_notary_status").innerHTML = "OK";
+		document.getElementById("remove_notary_sender").innerHTML = sender_address;
 	},
 };
 

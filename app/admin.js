4e5296dac5e762b3e2edd15602a0a240717cb977
kozyilmaz <kazim@monolytic.com>
2020-06-30 12:29:16 +0300

    js up

--- app/admin.js
+++ app/admin.js
@@ -1,10 +1,8 @@
-import { xyz_create_web3_provider, xyz_get_account_by_index, xyz_get_network_name, xyz_get_provider_name } from './common.js';
-import { xyz_ykts_get_contract, xyz_ykts_add_notary  } from './ykts.js';
+import { xyz_create_web3_provider, xyz_get_account_by_index, xyz_get_network_name, xyz_get_provider_name, xyz_get_accounts } from './common.js';
+import { xyz_ykts_get_contract, xyz_ykts_add_admin  } from './ykts.js';
 
 // YKTS contract interface
 var ykts_contract;
-// default address
-var default_address;
 
 window.App = {
 	start: async () => {
@@ -26,12 +24,6 @@ window.App = {
 			document.getElementById("note").innerHTML = "Metamask is available and Ethereum network is set to '" + network_name + "'";
 		} else {
 			document.getElementById("note").innerHTML = "Metamask is NOT available, aborting!";
-			return;
-		}
-		// get default address
-		default_address = await xyz_get_account_by_index(0);
-		if (default_address == null) {
-			alert("Unable to get default address, aborting!");
 			return;
 		}
 		// parse contract and get abi & address
@@ -66,18 +58,44 @@ window.App = {
 	add_admin: async () => {
 		var self = this;
 		document.getElementById("add_admin_status").innerHTML = "Pending";
-
+		// get new admin address
 		const new_admin_address = document.getElementById('new_admin_address')
 		if (!new_admin_address) {
+			document.getElementById("add_admin_status").innerHTML = "Failed";
 			alert("Empty admin address!");
 			return;
 		}
 		const address = new_admin_address.value;
-		// add admin
-		const response = await ykts_contract.methods.addAdmin(address).send({from: default_address});
-		console.log("Admin Add:", response);
 
+		// get tx sender address (current admin!)
+		const sender_address = await xyz_get_account_by_index(0);
+		if (sender_address == null) {
+			document.getElementById("add_admin_status").innerHTML = "Failed";
+			alert("Unable to get default address, aborting!");
+			return;
+		}
+		// add admin
+		const response = await xyz_ykts_add_admin(ykts_contract, sender_address, address);
+		console.log("Admin Add:", response);
+		// OK
 		document.getElementById("add_admin_status").innerHTML = "OK";
+	},
+
+	renounce_admin: async () => {
+		var self = this;
+		document.getElementById("renounce_admin_status").innerHTML = "Pending";
+		// get tx sender address (current admin!)
+		const sender_address = await xyz_get_account_by_index(0);
+		if (sender_address == null) {
+			document.getElementById("renounce_admin_status").innerHTML = "Failed";
+			alert("Unable to get default address, aborting!");
+			return;
+		}
+		// add admin
+		const response = await xyz_ykts_add_admin(ykts_contract, sender_address, address);
+		console.log("Admin Renounce:", response);
+		// OK
+		document.getElementById("renounce_admin_status").innerHTML = "OK";
 	},
 };
 

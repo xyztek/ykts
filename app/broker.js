c9d4f3785c3cf0eb71e4d10c05e5bbe558acb8b1
kozyilmaz <kazim@monolytic.com>
2020-06-30 14:48:02 +0300

    up

--- app/broker.js
+++ app/broker.js
@@ -1,7 +1,8 @@
-import { xyz_get_contract, xyz_ykts_broker_request, xyz_ykts_recover, xyz_ykts_is_signer, xyz_ykts_sign, xyz_create_web3_provider, xyz_get_balance, xyz_get_account_by_index, xyz_get_network_name, xyz_get_provider_name } from './common.js';
+import { xyz_create_web3_provider, xyz_get_account_by_index, xyz_get_network_name, xyz_get_provider_name } from './common.js';
+import { xyz_ykts_get_contract, xyz_ykts_sign, xyz_ykts_recover, xyz_ykts_broker_request } from './ykts.js';
 
-// default account
-var account;
+// YKTS contract interface
+var ykts_contract;
 
 window.App = {
 	start: async () => {
@@ -9,72 +10,82 @@ window.App = {
 
 		// get provider name
 		const provider = await xyz_get_provider_name();
-		if (provider === null) {
+		if (provider == null) {
 				alert("Couldn't get any Web3 providers, probably Metamask/Mist/Infura is not present!");
 				return;
 		}
+		const network_name = await xyz_get_network_name();
+		if (network_name == null) {
+			alert("Couldn't get Ethereum network name, aborting!");
+			return;
+		}
 		// check Metamask availability and report
 		if (web3.currentProvider.isMetaMask) {
-			document.getElementById("providernote").innerHTML = "Metamask is available, please set network to Rinkeby!"
+			document.getElementById("note").innerHTML = "Metamask is available and Ethereum network is set to '" + network_name + "'";
 		} else {
-			document.getElementById("providernote").innerHTML = "Metamask is NOT available, please use a local node or set 'infuraAPIKey' and connect to Rinkeby network! Some functionality will not work without Metamask";
+			document.getElementById("note").innerHTML = "Metamask is NOT available, aborting!";
+			return;
 		}
-		// get network name
-		const network = await xyz_get_network_name();
-		if (network === null) {
-			alert("Couldn't get any the Ethereum network, probably Metamask/Mist/Infura is not present!");
+		// parse contract and get abi & address
+		ykts_contract = await xyz_ykts_get_contract();
+		if (ykts_contract == null) {
+			alert("Unable to get ./build/YKTS.json smart contract on '" + network_name + "' network, aborting!");
 			return;
 		}
 	},
 
 	broker_request: async () => {
 		var self = this;
-		document.getElementById("brokerrequeststatus").innerHTML = "Pending";
-		document.getElementById("brokeraddress").innerHTML = 0;
-		document.getElementById("brokerid").innerHTML = 0;
-		document.getElementById("brokerhash").innerHTML = 0;
-		document.getElementById("brokersignature").innerHTML = 0;
+		document.getElementById("broker_request_status").innerHTML = "Pending";
+		document.getElementById("broker_address").innerHTML = 0;
+		document.getElementById("broker_id").innerHTML = 0;
+		document.getElementById("broker_hash").innerHTML = 0;
+		document.getElementById("broker_signature").innerHTML = 0;
 
-		const brokerid = document.getElementById('brokerappid')
-		if (!brokerid) {
+		// get tx sender address (current admin!)
+		const sender_address = await xyz_get_account_by_index(0);
+		if (sender_address == null) {
+			document.getElementById("broker_request_status").innerHTML = "Failed";
+			alert("Unable to get default address, aborting!");
+			return;
+		}
+		const id = document.getElementById('broker_appid').value;
+		if (!id) {
+			document.getElementById("broker_request_status").innerHTML = "Failed";
 			alert("Empty Id!");
 			return;
 		}
-		const id = brokerid.value;
-
-		const brokermessage = document.getElementById('brokermessage')
-		if (!brokermessage) {
+		const message = document.getElementById('broker_message').value;
+		if (!message) {
+			document.getElementById("broker_request_status").innerHTML = "Failed";
 			alert("Empty message!");
 			return;
 		}
-		const message = brokermessage.value;
 		console.log("Broker Message:", message);
 		// calculate hash of message
 		const hash = web3.utils.sha3(message);
 		console.log("Broker Hash:", hash);
 
-		// sign hash of message with default account
-		const address = await xyz_get_account_by_index(0);
-		const signature = await xyz_ykts_sign(hash, address);
+		const signature = await xyz_ykts_sign(ykts_contract, sender_address, hash);
 		console.log("Broker Signature: ", signature);
 
 		// recover signer address
-		const recover = await xyz_ykts_recover(hash, signature);
+		const recover = await xyz_ykts_recover(ykts_contract, hash, signature);
 		console.log("Broker Recover: ", recover);
 
-		if (address != recover) {
+		if (sender_address != recover) {
 			alert("Address/Recover mismatch!");
 			return;
 		}
 		// request for approval
-		const response = await xyz_ykts_broker_request(address, id, hash, signature);
+		const response = await xyz_ykts_broker_request(ykts_contract, sender_address, id, hash, signature);
 		console.log("Broker Response: ", response)
 
-		document.getElementById("brokerrequeststatus").innerHTML = response.status;
-		document.getElementById("brokeraddress").innerHTML = address;
-		document.getElementById("brokerid").innerHTML = id;
-		document.getElementById("brokerhash").innerHTML = hash;
-		document.getElementById("brokersignature").innerHTML = signature;
+		document.getElementById("broker_request_status").innerHTML = response.status;
+		document.getElementById("broker_address").innerHTML = sender_address;
+		document.getElementById("broker_id").innerHTML = id;
+		document.getElementById("broker_hash").innerHTML = hash;
+		document.getElementById("broker_signature").innerHTML = signature;
 	},
 
 };
